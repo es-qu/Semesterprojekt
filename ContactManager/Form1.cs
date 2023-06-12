9c2e2bd41fe2cc8ff416df50a894ef2f81b23489
Maximilian Degen <maximiliandegen.hotmail.com@zbw-online.ch>
2023-06-12 15:47:19 +0200

    Steven Test Push as second

--- ContactManager/Form1.cs
+++ ContactManager/Form1.cs
@@ -16,7 +16,7 @@ namespace ContactManager
         {
             InitializeComponent();
             // Elias
-            // Steven
+            // Steven die Wurst!
         }
 
         private void Form1_Load(object sender, EventArgs e)
