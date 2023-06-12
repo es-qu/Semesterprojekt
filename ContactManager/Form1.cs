6507d9b1268c977d0894ae020c7770a5822e92f5
Maximilian Degen <maximiliandegen.hotmail.com@zbw-online.ch>
2023-06-12 15:42:07 +0200

    Push it real good

--- ContactManager/Form1.cs
+++ ContactManager/Form1.cs
@@ -16,5 +16,10 @@ namespace ContactManager
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //El Hamstergruesch was here!!!
+        }
     }
 }
